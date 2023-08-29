28fdb8346b7f6c24bac0e5748c5139b99944a6e4
luck07051 <luck0751@gmail.com>
2023-08-29 17:31:39 +0800

    function key

--- keymap.c
+++ keymap.c
@@ -28,8 +28,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_FUN_] = LAYOUT(
         XXXXXXX, _______, _______, _______, QK_BOOT,      _______, _______, _______, _______, XXXXXXX,
-        _______, KC_F12,  KC_F11,  KC_F10,  _______,      _______, KC_F1,   _______, _______, _______,
-        KC_F6,   KC_F7,   KC_F8,   KC_F9,   _______,      _______, KC_F2,   KC_F3,   KC_F4,   KC_F5,
+        KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,       KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,
+        _______, _______, _______, KC_F6,   _______,      _______, KC_F5,   _______, _______, _______,
                                    XXXXXXX, _______,      _______, XXXXXXX
     ),
 
