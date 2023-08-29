6fa3936c5fe0aa017a5b8d73945675307e0e31d3
luck07051 <luck0751@gmail.com>
2023-08-29 17:27:57 +0800

    add some combo

--- keymap.c
+++ keymap.c
@@ -21,8 +21,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_NUM_] = LAYOUT(
         XXXXXXX, KC_PLUS, KC_MINS, KC_EQL,  ST_HMD,       _______, KC_ASTR, KC_SLSH, KC_CIRC, XXXXXXX,
-        KC_6,    KC_4,    KC_0,    KC_2,    KC_GRV,       KC_AT,   KC_3,    KC_1,    KC_5,    KC_7,
-        KC_UNDS, KC_BSLS, KC_PERC, KC_8,    KC_TILD,      _______, KC_9,    KC_DLR,  KC_COMM, KC_DOT,
+        KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,       KC_AT,   KC_1,    KC_2,    KC_3,    KC_4,
+        KC_UNDS, KC_BSLS, KC_PERC, KC_6,    KC_TILD,      _______, KC_5,    KC_DLR,  KC_COMM, KC_DOT,
                                    XXXXXXX, _______,      _______, XXXXXXX
     ),
 
